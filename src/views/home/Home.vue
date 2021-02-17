e35a702bac649ccd203a56f7b2559dab04bfd3a5
yangyangyang-hub <962369014@qq.com>
2021-02-17 22:25:58 +0800

    首页导航栏封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-      <h2>首页</h2>
+      <nav-bar class="home-bar">
+        <template v-slot:center>
+          <div>购物街</div>
+        </template>
+      </nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from '../../components/common/navbar/NavBar'
 export default {
-
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style>
-
+.home-bar {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
