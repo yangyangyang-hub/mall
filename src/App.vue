38a3bb96be00517e572c3bcba43a385c0b7a695a
yangyangyang-hub <962369014@qq.com>
2021-02-17 09:33:46 +0800

    tabbar和路由结合完成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,72 @@
 <template>
-  
+  <div id="app">
+    <tab-bar>
+
+      <tab-bar-item path="/home">
+        <template v-slot:item-icon>
+          <img src="./assets/img/tabbar/1.jpg" alt="" />
+        </template>
+        <template v-slot:item-icon-active>
+          <img src="./assets/img/tabbar/2.jpg" alt="" />
+        </template>
+        <template v-slot:item-text>
+          <div>首页</div>
+        </template>
+      </tab-bar-item>
+
+      <tab-bar-item path="/category">
+        <template v-slot:item-icon>
+          <img src="./assets/img/tabbar/1.jpg" alt="" />
+        </template>
+        <template v-slot:item-icon-active>
+          <img src="./assets/img/tabbar/2.jpg" alt="" />
+        </template>
+        <template v-slot:item-text>
+          <div>分类</div>
+        </template>
+      </tab-bar-item>
+
+      <tab-bar-item path="/cart">
+        <template v-slot:item-icon>
+          <img src="./assets/img/tabbar/1.jpg" alt="" />
+        </template>
+        <template v-slot:item-icon-active>
+          <img src="./assets/img/tabbar/2.jpg" alt="" />
+        </template>
+        <template v-slot:item-text>
+          <div>购物车</div>
+        </template>
+      </tab-bar-item>
+
+      <tab-bar-item path="profile">
+        <template v-slot:item-icon>
+          <img src="./assets/img/tabbar/1.jpg" alt="" />
+        </template>
+        <template v-slot:item-icon-active>
+          <img src="./assets/img/tabbar/2.jpg" alt="" />
+        </template>
+        <template v-slot:item-text>
+          <div>我的</div>
+        </template>
+      </tab-bar-item>
+    </tab-bar>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import TabBar from "./components/common/tabbar/TabBar";
+import TabBarItem from "./components/common/tabbar/TabBarItem";
+
 export default {
-    name: 'app',
-    components: {
-        
-    }
-}
+  name: "app",
+  components: {
+    TabBar,
+    TabBarItem,
+  },
+};
 </script>
 
 <style>
-    @import "./assets/css/base.css";
+@import "./assets/css/base.css";
 </style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+
+    <div v-else>
+      <slot name="item-text-active"></slot>
+    </div>
+
+    <div :class="{ active: isActive }">
+      <slot name="item-text"></slot>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+  props: {
+      path:String
+  },
+  methods: {
+      itemClick() {
+          this.$router.push(this.path)
+      }
+  }
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+}
+.active {
+  color: red;
+}
+</style>
