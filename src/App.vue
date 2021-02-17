154c6ff5028f1b139958082df50f4a0d01268de9
yangyangyang-hub <962369014@qq.com>
2021-02-17 20:01:18 +0800

    tabbar图片更改

--- src/App.vue
+++ src/App.vue
@@ -2,48 +2,48 @@
   <div id="app">
     <tab-bar>
 
-      <tab-bar-item path="/home" activeColor="red">
+      <tab-bar-item path="/home" activeColor="deepPink">
         <template v-slot:item-icon>
-          <img src="./assets/img/tabbar/1.jpg" alt="" />
+          <img src="./assets/img/tabbar/home.svg" alt="" />
         </template>
         <template v-slot:item-text-active>
-          <img src="./assets/img/tabbar/2.jpg" alt="" />
+          <img src="./assets/img/tabbar/home_active.svg" alt="" />
         </template>
         <template v-slot:item-text>
           <div>首页</div>
         </template>
       </tab-bar-item>
 
-      <tab-bar-item path="/category" activeColor="red">
+      <tab-bar-item path="/category" activeColor="deepPink">
         <template v-slot:item-icon>
-          <img src="./assets/img/tabbar/1.jpg" alt="" />
+          <img src="./assets/img/tabbar/category.svg" alt="" />
         </template>
         <template v-slot:item-text-active>
-          <img src="./assets/img/tabbar/2.jpg" alt="" />
+          <img src="./assets/img/tabbar/category_active.svg" alt="" />
         </template>
         <template v-slot:item-text>
           <div>分类</div>
         </template>
       </tab-bar-item>
 
-      <tab-bar-item path="/cart" activeColor="red">
+      <tab-bar-item path="/cart" activeColor="deepPink">
         <template v-slot:item-icon>
-          <img src="./assets/img/tabbar/1.jpg" alt="" />
+          <img src="./assets/img/tabbar/cart.svg" alt="" />
         </template>
         <template v-slot:item-text-active>
-          <img src="./assets/img/tabbar/2.jpg" alt="" />
+          <img src="./assets/img/tabbar/cart_active.svg" alt="" />
         </template>
         <template v-slot:item-text>
           <div>购物车</div>
         </template>
       </tab-bar-item>
 
-      <tab-bar-item path="profile" activeColor="red">
+      <tab-bar-item path="profile" activeColor="deepPink">
         <template v-slot:item-icon>
-          <img src="./assets/img/tabbar/1.jpg" alt="" />
+          <img src="./assets/img/tabbar/profile.svg" alt="" />
         </template>
         <template v-slot:item-text-active>
-          <img src="./assets/img/tabbar/2.jpg" alt="" />
+          <img src="./assets/img/tabbar/profile_active.svg" alt="" />
         </template>
         <template v-slot:item-text>
           <div>我的</div>
--- src/components/common/tabbar/TabBarItem.vue
+++ src/components/common/tabbar/TabBarItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tab-bar-item" @click="itemClick">
-    <div v-if="isActive">
+    <div v-if="!isActive">
       <slot name="item-icon"></slot>
     </div>
 
