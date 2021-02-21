716b7757be09b1cf0f3198955e50f61083958b48
yangyangyang-hub <962369014@qq.com>
2021-02-21 19:00:31 +0800

    scorll组件封装

---
+++ src/components/common/scroll/scorll.vue
@@ -0,0 +1,32 @@
+<template>
+  <div ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import BScroll from "@better-scroll/core";
+// import Pullup from "@better-scroll/pull-up";
+// BScroll.use(Pullup);
+
+export default {
+  data() {
+    return {
+      scroll: null,
+    };
+  },
+  mounted() {
+      this.scroll = new BScroll(this.$refs.wrapper, {
+
+      })
+      console.log(this.scroll);
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,18 @@
-<template>
+.<template>
   <div id="home">
     <nav-bar class="home-bar">
       <template v-slot:center>
         <div>购物街</div>
       </template>
     </nav-bar>
-    <swiper :banner="banners"></swiper>
-    <recommend-view :recommends="recommends"></recommend-view>
-    <feature-view />
-    <tab-control class="tab-control" @tabclick="tabclick"></tab-control>
-    <goods-list :goods="goods[currentType].list"></goods-list>
+
+    <scroll class="scroll">
+      <swiper :banner="banners"></swiper>
+      <recommend-view :recommends="recommends"></recommend-view>
+      <feature-view />
+      <tab-control class="tab-control" @tabclick="tabclick"></tab-control>
+      <goods-list :goods="goods[currentType].list"></goods-list>
+    </scroll>
   </div>
 </template>
 
@@ -17,11 +20,12 @@
 import { getHomeMultidata, goodHomeGoods } from "../../network/home";
 
 import NavBar from "../../components/common/navbar/NavBar";
-import GoodsList from '../../components/content/goods/GoodsList'
+import GoodsList from "../../components/content/goods/GoodsList";
 import Swiper from "./childComponents/Swiper";
 import RecommendView from "./childComponents/RecommendView";
 import FeatureView from "./childComponents/FeatureView";
 import TabControl from "../../components/content/tabControl/TabControl";
+import Scroll from "../../components/common/scroll/scorll";
 
 export default {
   components: {
@@ -31,17 +35,18 @@ export default {
     FeatureView,
     TabControl,
     GoodsList,
+    Scroll,
   },
   data() {
     return {
       banners: [],
       recommends: [],
       goods: {
-        'pop': { page: 0, list: [] },
-        'new': { page: 0, list: [] },
-        'sell': { page: 0, list: [] },
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
       },
-      currentType: 'pop'
+      currentType: "pop",
     };
   },
   created() {
@@ -53,37 +58,36 @@ export default {
         this.recommends = res.data.recommend.list;
       }
     });
-    this.goodHomeGoods('pop')
-    this.goodHomeGoods('new')
-    this.goodHomeGoods('sell')
-
+    this.goodHomeGoods("pop");
+    this.goodHomeGoods("new");
+    this.goodHomeGoods("sell");
   },
   methods: {
-
     tabclick(index) {
-      if(index == 0) {
-        this.currentType = 'pop'
-      } else if(index == 1) {
-        this.currentType = 'new'
-      } else{
-        this.currentType = 'sell'
+      if (index == 0) {
+        this.currentType = "pop";
+      } else if (index == 1) {
+        this.currentType = "new";
+      } else {
+        this.currentType = "sell";
       }
     },
 
     goodHomeGoods(type) {
-      const page = this.goods[type].page + 1
+      const page = this.goods[type].page + 1;
       goodHomeGoods(type, page).then((res) => {
-        this.goods[type].list.push(...res.data.list)
-        this.goods[type].page += 1
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
       });
     },
   },
 };
 </script>
 
-<style>
+<style >
 #home {
   padding-top: 44px;
+  height: 100vh;
 }
 .home-bar {
   background-color: var(--color-tint);
@@ -99,4 +103,9 @@ export default {
   position: sticky;
   top: 44px;
 }
+.scroll {
+  height: calc(100% - 49px);
+  overflow: hidden;
+}
+
 </style>
