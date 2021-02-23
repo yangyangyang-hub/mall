c068a38a3b60cfececfaadd254722ca80a88e90a
yangyangyang-hub <962369014@qq.com>
2021-02-23 21:04:18 +0800

    详情页轮播图展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,31 +1,34 @@
 <template>
   <div>
     <detail-nav-bar></detail-nav-bar>
+    <detail-swiper :topImages="topImages" />
     {{ id }}
   </div>
 </template>
 
 <script>
 import {getDetail} from '../../network/detail'
+
 import DetailNavBar from "./childCompents/DetailNavBar";
+import DetailSwiper from './childCompents/DetailSwiper'
 
 export default {
   components: {
     DetailNavBar,
+    DetailSwiper,
   },
   data() {
     return {
       id: null,
       titles: ["商品", "参数", "评论", "推荐"],
+      topImages: [],
     };
   },
   created() {
     this.id = this.$route.query.id;
-    console.log(this.id);
-    console.log(getDetail);
 
     getDetail(this.id).then(res => {
-      console.log(res);
+      this.topImages = res.result.itemInfo.topImages
     })
   },
 };
---
+++ src/views/detail/childCompents/DetailSwiper.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="item">
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="pink">
+      <van-swipe-item v-for="(item, index) in topImages" :key="index">
+        <img :src="item" alt="" />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    topImages: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.my-swipe {
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  height: 300px;
+  text-align: center;
+  background-color: #39a9ed;
+}
+img {
+  width: 100%;
+  vertical-align: bottom;
+}
+</style>>
