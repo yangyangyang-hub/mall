3d0df770494cc71981c2dfaa64cac54d5af79a35
yangyangyang-hub <962369014@qq.com>
2021-02-23 22:42:10 +0800

    数据结构封装

--- src/network/detail.js
+++ src/network/detail.js
@@ -8,3 +8,13 @@ export function getDetail (iid) {
         }
     })
 }
+
+export class Goods {
+    constructor(itemInfo, columns, service) {
+        this.desc = itemInfo.desc;
+        this.price = itemInfo.price;
+        this.oldPrice = itemInfo.oldPrice;
+        this.discount = itemInfo.discountDesc;
+        this.service = service
+    }
+}
